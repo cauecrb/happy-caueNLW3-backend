47d421df1dd959920f4809c09e8c9dc8d4b68d55
cauecrb <crburgardt@gmail.com>
2020-11-07 16:58:37 -0300

    aws storage

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -1,9 +1,11 @@
-import { request } from 'express';
+//import { request } from 'express';
 import multer from 'multer';
 import path from 'path';
+import multerS3 from 'multer-s3';
+import aws from 'aws-sdk';
 
 export default{
-    storage: multer.diskStorage({
+/*     storage: multer.diskStorage({
         destination: path.join(__dirname,'..','..','uploads'),
         filename:(request, file, cb) =>{
             const filename = `${Date.now()}-${file.originalname}`;
@@ -11,5 +13,18 @@ export default{
 
             cb(null, filename);
         },
-    })
+    }) */
+
+         storage: multerS3({
+             s3: new aws.S3(),
+             bucket: 'happycaue',
+             contentType: multerS3.AUTO_CONTENT_TYPE,
+             acl: "public-read",
+            key: (request, file, cb) =>{
+                const filename = `${Date.now()}-${file.originalname}`;
+                
+    
+                cb(null, filename);
+             },
+         }),
 }
